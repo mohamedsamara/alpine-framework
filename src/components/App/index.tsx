cd7b923bf2fa2afc11e9bce5c475d27911c1c758
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2022-07-10 15:20:26 -0700

    feat: add tailwindcss

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,9 +4,11 @@ import Counter from "../Counter";
 
 const App = () => {
   return (
-    <div>
-      <h1>Alpine Framework</h1>
-      <p>Using Alpine with TSX syntax</p>
+    <div class="flex flex-col justify-center items-center h-screen">
+      <h1 class="text-2xl font-semibold text-center">Alpine Framework</h1>
+      <p class="text-lg text-gray-500 mt-2 mb-6 text-center">
+        Using Alpine with TSX syntax
+      </p>
       <Counter />
     </div>
   );
--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -2,10 +2,26 @@ import { h } from "tsx-dom";
 
 const Counter = () => {
   return (
-    <div x-data="{ count: 0 }">
-      <button x-on:click="count++">+</button>
-      <span x-text="count"></span>
-      <button x-on:click="count--">-</button>
+    <div
+      class="flex flex-row justify-center items-center"
+      x-data="{ count: 0 }"
+    >
+      <button
+        class="bg-gray-200 hover:bg-gray-300 w-12 p-2 rounded-sm transition-all	duration-300"
+        x-on:click="count++"
+      >
+        +
+      </button>
+      <span
+        class="text-center min-w-fit w-12 mx-4 text-2xl font-medium"
+        x-text="count"
+      ></span>
+      <button
+        class="bg-gray-200 hover:bg-gray-300 w-12 p-2 rounded-sm transition-all	duration-300"
+        x-on:click="count--"
+      >
+        -
+      </button>
     </div>
   );
 };
